eaa7672ff2db5771cbfe6022c02027b344e4a866
TheFlamingCrab <96930137+TheFlamingCrab@users.noreply.github.com>
2022-05-29 00:01:21 +0800

    created contact class

---
+++ Contact.cs
@@ -0,0 +1,20 @@
+namespace bardchat
+{
+    public class Contact
+    {
+        public string name;
+        public Guid id;
+        
+        public Contact(string name, Guid id)
+        {
+            this.name = name;
+            this.id = id;
+        }
+
+        public Contact(Guid id)
+        {
+            this.id = id;
+            this.name = id.ToString();
+        }
+    }
+}
